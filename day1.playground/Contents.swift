88ca7ca5b68b08a70a09cf8790ffcb1cee0b0345
ioschristian <christian.d.tisby@gmail.com>
2021-12-21 23:53:32 -0500

    first commit

---
+++ day1.playground/Contents.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+//variables
+var str = "Hello, playground"
+str = "Goodbye"
+
+
+
+var favoriteShow = "Orange is the New Black"
+favoriteShow = "The Good Place"
+favoriteShow = "Doctor Who"
+
+//multiline strings
+var str1 = """
+This goes over multiple
+lines
+"""
+
+//Strings and Integers
+var streeet = "Bankroll Fresh"
+var age = 39
+var population = 8_000_000
+
+//Doubles and Booleans
+
+var pi = 3.141
+var awesome = true
+
+//String Interpolation
+var score = 85
+var newStr = "Your score was \(score)"
+var results = "The test results are here: \(str)"
+
+//Constants = always use let unless you specifically want to change a variable
+let taylor = "Swift"
+
+//Type annotations
+let str2: String = "Again"
+let plentyMoney: Int = 2_000_000
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
