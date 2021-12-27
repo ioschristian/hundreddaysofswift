cdc2dad3acee1b2967d4b64b26d3bb3a0515c514
ioschrisitan <christian.d.tisby@gmail.com>
2021-12-27 00:44:37 -0500

    complete day 5

--- day5.playground/Contents.swift
+++ day5.playground/Contents.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Foundation
+import CoreFoundation
 
 // Functions
 
@@ -15,7 +16,89 @@ myApp will resize them all into thumbmails
 
 printHelp()
 
+//Accepting Parameters
 
+print("Hello world")
 
+func square(number: Int) {
+    print(number * number)
+}
+
+square(number: 8)
+
+// Returning Values
+
+func square2(number: Int) -> Int {
+    return number * number
+}
+
+let result = square2(number: 8)
+print(result)
+
+func sayHello(to name: String) {
+    print("Hello, \(name)!")
+}
+
+sayHello(to: "Taylor")
+
+func greet(_ person: String) {
+    print("Hello, \(person)!")
+}
+
+greet("Taylor")
+
+// Default parameters
+
+func greet2(_ person: String, nicely: Bool = true) {
+    if nicely == true {
+        print("Hello, \(person)!")
+    } else {
+        print("Oh no, it's \(person) again...")
+    }
+    
+}
+
+greet2("Taylor")
+greet2("Taylor", nicely: false)
+
+// Variadic functions
+// You create variadic functions by placing 3 dots after the function parameter type
+
+func square3(numbers: Int...){
+    for number in numbers {
+        print("\(number) squared is \(number * number)")
+    }
+}
+
+square3(numbers: 1, 2, 3, 4, 5)
+
+//  Writing throwing functions
+
+enum PasswordError: Error {
+    case obvious
+}
+
+func checkPassword(_ password: String) throws -> Bool {
+    if password == "password" {
+        throw PasswordError.obvious
+    }
+    
+    return true
+}
+
+// Running throwing functions
+do {
+    try checkPassword("password")
+    print("That password is good!")
+} catch {
+    print("You can't use that password.")
+}
+
+func doubleInPlace(number: inout Int) {
+    number *= 2
+}
+
+var myNum = 10
+doubleInPlace(number: &myNum)
 
 
