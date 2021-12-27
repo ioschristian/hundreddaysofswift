9f6520ce28dd4d752b32d88a2469249d5e219df1
ioschrisitan <christian.d.tisby@gmail.com>
2021-12-26 23:52:50 -0500

    adding day 5

---
+++ day5.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+import Foundation
+
+// Functions
+
+func printHelp() {
+    let message = """
+Welcome  to MyApp!
+
+Run this app inside a directory of images and
+myApp will resize them all into thumbmails
+"""
+    print(message)
+}
+
+printHelp()
+
+
+
+
+
