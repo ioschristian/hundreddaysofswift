d0be8cd242418d3c9d7b2be53f7f164c72cd5c7b
ioschrisitan <christian.d.tisby@gmail.com>
2021-12-29 09:53:22 -0500

    adding day 1 Swift UI

---
+++ day1swiftui.playground/Contents.swift
@@ -0,0 +1,95 @@
+import UIKit
+
+
+var greeting = "Hello, playground"
+var name = "Ted"
+name = "Rebecca"
+name = "Keeley"
+
+let character = "Daphne"
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
+let managerName = "Michael Scott"
+let dogBreed = "Samoyed"
+let meaningOfLife = "How many roads must a man walk down?"
+
+
+let actor = "Denzel Washington"
+
+let filename = "paris.jpg"
+
+let result = "You win!"
+
+let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+
+let movie = """
+A day in the life of an
+Apple engineer
+"""
+
+print(actor.count)
+
+let nameLength = actor.count
+print(nameLength)
+
+print(result.uppercased())
+print(movie.hasPrefix("A day"))
+
+print(filename.hasSuffix(".jpg"))
+
+// Whole numbers
+let score = 10
+
+let reallyBig = 100_000_000
+
+let lowerScore = score - 2
+let higherScore = score + 10
+let doubledScore = score * 2
+let squaredScore = score * score
+let halvedScore = score / 2
+
+print(score)
+
+var counter = 10
+counter = counter + 5
+
+counter += 5
+print(score)
+
+counter *= 2
+print(score)
+
+counter -= 10
+print(score)
+
+counter /= 2
+print(score)
+
+let number = 120
+print(number.isMultiple(of: 3))
+
+// Decimal numbers
+let number2 = 0.1 + 0.2
+print(number2)
+
+let a = 1
+let b = 2.0
+//let c = a + b
+
+let c = a + Int(b)
+
+// Or let c = Double(a) + b
+
+
+
+
+
+
