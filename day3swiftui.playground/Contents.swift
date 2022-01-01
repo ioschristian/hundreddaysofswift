97b44bc00d9faa238140a4a543cb90eb64a95d3e
ioschristian <christian.d.tisby@gmail.com>
2021-12-31 23:56:51 -0500

    updating day 3

--- day3swiftui.playground/Contents.swift
+++ day3swiftui.playground/Contents.swift
@@ -43,3 +43,76 @@ print(cities.sorted())
 let presidents = ["Bush", "Obama", "Trump", "Biden"]
 let reversedPresidents = presidents.reversed()
 print(reversedPresidents)
+
+//  How to Store and find data in dictionaries
+var employee = ["Taylor Swift", "Singer", "Nashville"]
+print("Name: \(employee[0])")
+print("Job title: \(employee[1])")
+print("Location: \(employee[2])")
+
+
+//Dictionaries
+let employee2 = [
+    "name": "Taylor Swift",
+    "job": "Singer",
+    "location": "Nashville"
+]
+
+print(employee2["name",default: "Unknown"])
+print(employee2["job", default: "Unknown"])
+print(employee2["location", default: "Unknown"])
+
+
+let hasGraduated = [
+    "Eric": false,
+    "Maeve": true,
+    "Otis": false
+]
+
+
+let olympics = [
+    2012: "London",
+    2016: "Rio de Janeiro",
+    2021: "Tokyo"
+]
+
+print(olympics[2012, default: "Unknown"])
+
+
+var heights = [String: Int]()
+heights["Yao Ming"] = 229
+heights["Shaquille O'Neal"] = 216
+heights["Lebron James"] = 206
+
+var archEnemies = [String: String]()
+archEnemies["Batman"] = "The Joker"
+archEnemies["Superman"] = "Lex Luthor"
+
+
+archEnemies["Batman"] = "Penguin"
+
+//Sets
+//let people = Set(["Denzel Washington", "Tom Cruise", "Nicolas Cage", "Samuel L Jackson"])
+//print(people)
+
+var people = Set<String>()
+people.insert("Denzel Washington")
+people.insert("Tom Cruise")
+people.insert("Nicolas Cage")
+people.insert("Samuel L Jackson")
+
+
+//Enums
+
+enum Weekday {
+    case monday
+    case tuesday
+    case wednesday
+    case thursday
+    case friday
+}
+
+
+var day = Weekday.monday
+day = Weekday.tuesday
+day = Weekday.thursday
