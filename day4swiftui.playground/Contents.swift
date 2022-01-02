9dc7a3bb0468f8fff8259f4a5235585f69734339
ioschristian <christian.d.tisby@gmail.com>
2022-01-01 22:31:41 -0500

    completing day 4

---
+++ day4swiftui.playground/Contents.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+// How to use type annotations
+
+let surname: String = "Lasso"
+var score: Double = 0
+
+let playerName: String = "Roy"
+var luckyNumber: Int = 13
+
+let pi: Double = 3.141
+
+var isAuthenticated: Bool = true
+
+//Array
+var albums: [String] = ["Red", "Fearless"]
+
+//Dictionary
+var user: [String: String] = ["id": "@twostraws"]
+
+//Sets
+var books: Set<String> = Set(["The Bluest Eye", "Foundation", "Girl, Woman, Other"])
+
+// Create an empty array of Strings
+var teams: [String] = [String]()
+
+var clues = [String]()
+
+//Enums
+
+enum UIStyle {
+    case light, dark, system
+}
+
+var style = UIStyle.light
+
+
+
+
