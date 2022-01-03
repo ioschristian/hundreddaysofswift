2aa6da967b4dfe8a23fe3549252d83fb3823086e
ioschristian <christian.d.tisby@gmail.com>
2022-01-02 20:05:21 -0500

    completed day 5

--- day5swiftui.playground/Contents.swift
+++ day5swiftui.playground/Contents.swift
@@ -89,3 +89,163 @@ if username.isEmpty == true {
 if username.isEmpty {
     username = "Anonymous"
 }
+
+
+let age2 = 16
+
+if age2 >= 18 {
+    print("You can vote in the next election.")
+}
+
+if age2 < 18 {
+    print("Sorry, you're too young to vote.")
+}
+
+let age3 = 16
+
+if age3 >= 18 {
+    print("You can vote in the next election.")
+} else {
+    print("Sorry, you're too young to vote.")
+}
+
+
+let a = false
+let b = true
+
+if a {
+    print("Code to run if a is true")
+} else if b {
+    print("Code to run if a is false but b is true")
+} else {
+    print("Code to run if both a and b are false")
+}
+
+let temp = 25
+if temp > 20 {
+    if temp < 30 {
+        print("It's a nice day.")
+    }
+}
+
+if temp > 20 && temp < 30 {
+    print("It's a nice day.")
+}
+
+let userAge = 14
+let hasParentalConsent = true
+if userAge >= 18 || hasParentalConsent {
+    print("You can buy the game")
+}
+
+
+enum TransportOption {
+    case airplane, helicopter, bicycle, car, scooter
+}
+
+let transport = TransportOption.airplane
+
+if transport == .airplane || transport == .helicopter {
+    print("Let's fly")
+} else if transport == .bicycle {
+    print("I hope there's a bike path...")
+} else if transport == .car {
+    print("Time to get stuck in traffic.")
+} else {
+    print("I'm going to hire a scooter now!")
+}
+
+
+enum Weather {
+    case sun, rain, wind, snow, unknown
+}
+
+let forecast = Weather.sun
+
+if forecast == .sun {
+    print("It should be a nice day.")
+} else if forecast == .rain {
+    print("Pack an umbrella.")
+} else if forecast == .wind {
+    print("Wear something warm")
+} else if forecast == . rain {
+    print("School is cancelled.")
+} else {
+    print("Our forecast generator is broken!")
+}
+
+switch forecast {
+case .sun:
+    print("It should be a nice day.")
+case .rain:
+    print("Pack an umbrella")
+case .wind:
+    print("Wear something warm")
+case .snow:
+    print("School is cancelled")
+case .unknown:
+    print("Our forecast generator is broken!")
+    
+}
+
+let place = "Metropolis"
+
+switch place {
+case "Gotham":
+    print("You're Batman")
+case "Mega-City One":
+    print("You're Judge Dredd!")
+case "Wakanda":
+    print("You're Black Panther!")
+default:
+    print("Who are you?")
+}
+
+
+let day = 5
+print("My true love gave to me...")
+
+switch day {
+case 5:
+    print("5 golden rings")
+    fallthrough
+case 4:
+    print("4 calling birds")
+    fallthrough
+case 3:
+    print("3 french hens")
+    fallthrough
+case 2:
+    print("2 turtle doves")
+    fallthrough
+default:
+    print("A partridge in a pear tree")
+}
+
+
+
+let age4 = 18
+let canVote = age >= 18 ? "Yes" : "No"
+
+let hour = 23
+print(hour < 12 ? "It's before noon" : "It's after noon")
+
+let names = ["Jayne", "Kaylee", "Mal"]
+let crewCount = names.isEmpty ? "No one" : "\(names.count) people"
+print(crewCount)
+
+enum Theme {
+    case light, dark
+}
+
+
+let theme = Theme.dark
+
+
+let background = theme == .dark ? "black" : "white"
+print(background)
+
+let hour2 = 23
+print(hour2 < 12 ? "It's before noon" : "It's after noon")
+
+
