79b9915e8c2bbd3b959f08a2e1d770b45f4a0382
ioschristian <christian.d.tisby@gmail.com>
2022-01-03 20:18:47 -0500

    adding day 6 complete

---
+++ day6swiftui.playground/Contents.swift
@@ -0,0 +1,113 @@
+import UIKit
+
+let platforms = ["iOS", "macOS", "tvOS", "watchOS"]
+
+for os in platforms {
+    print("Swift works great on \(os).")
+}
+//
+//for i in 1...12 {
+//    print("5 x \(i) is \(5 * i)")
+//}
+//
+//
+
+for i in 1...12 {
+    print("The \(i) times table:")
+    
+    for j in 1...12 {
+        print(" \(j) x \(i) is \(j * i)")
+    }
+    
+    print()
+}
+
+for i in 1...5 {
+    print("Counting from 1 through 5: \(i)")
+}
+
+print()
+
+for i in 1..<5 {
+    print("Counting 1 up to 5: \(i)")
+}
+
+var lyric = "Haters gonna"
+
+for _ in 1...5 {
+    lyric += " hate"
+}
+
+print(lyric)
+
+var countdown = 10
+
+while countdown > 0 {
+    print("\(countdown)...")
+    countdown -= 1
+}
+
+print("Blast off!")
+
+let id = Int.random(in: 1...1000)
+
+let amount = Double.random(in: 0...1)
+
+
+// create an integer to store our roll
+var roll = 0
+
+// carry on looping until we reach 20
+while roll != 20 {
+    // roll a new dice and print what it was
+    roll = Int.random(in: 1...20)
+    print("I rolled a \(roll)")
+}
+
+// if we're here it means the loop ended - we got a 20!
+print("Critical hit!")
+
+let fileNames = ["me.jpg", "work.txt", "sophie.jpg", "logo.psg"]
+
+
+
+
+
+for filename in fileNames {
+    if filename.hasSuffix(".jpg") == false {
+        continue
+    }
+    
+    print("Found picture: \(filename)")
+    
+}
+
+let number1 = 4
+let number2 = 14
+var multiples = [Int]()
+
+for i in 1...100_000 {
+    if i.isMultiple(of: number1) && i.isMultiple(of: number2) {
+        multiples.append(i)
+        
+        if multiples.count == 10 {
+            break
+        }
+    }
+}
+
+print(multiples)
+
+
+
+for i in 1...100 {
+    if i % 3 == 0 {
+        print("Fuck")
+    } else if i % 5 == 0 {
+        print("Suck a duck")
+    } else if i % 3 == 0 && i % 5 == 0 {
+        print("Fuck and Suck a Duck")
+    } else {
+        print(i)
+    }
+}
