a93ff6189486e6dadd3611ba87dbf3e40d034d74
ioschristian <christian.d.tisby@gmail.com>
2022-01-04 20:27:43 -0500

    completed day 7 functions

--- day7swiftui.playground/Contents.swift
+++ day7swiftui.playground/Contents.swift
@@ -37,5 +37,57 @@ func printTimesTables(number: Int, end: Int) {
 printTimesTables(number: 5, end: 10)
 
 
+let root = sqrt(169)
+print(root)
 
+func rollDice() -> Int {
+    return Int.random(in: 1...6)  // return keyword not necessary when a function only has one line of code
+}
+
+let result = rollDice()
+print(result)
+
+func areLettersIdentical(string1: String, string2: String) -> Bool {
+    let first = string1.sorted()
+    let second = string2.sorted()
+    return first == second  // the return key is not necessary, could do
+    // string1.sorted() == string2.sorted()
+}
+
+
+
+func pythagoras(a: Double, b: Double) -> Double {
+    let input = a * a + b * b
+    let root = sqrt(input)
+    return root // could skip it all and do sqrt(a * a + b * b)
+}
+
+let c = pythagoras(a: 3, b: 4)
+print(c)
+
+func isUppercase(string: String) -> Bool {
+    string == string.uppercased()
+}
+
+// returning two or more values from a function
+
+//func getUser() -> [String] {
+//    ["Taylor", "Swift"]
+//}
+//
+//let user = getUser()
+//print("Name: \(user[0]) \(user[1])")
+
+
+// Use Tuples
+
+func getUser() -> (firstName: String, lastName: String) {
+    (firstName: "Taylor", lastName: "Swift")  // can remove the parameter names given in the tuple
+}
+
+let user = getUser()
+print("Name: \(user.firstName) \(user.lastName)")
+
+
+// How to customize parameter labels
 
