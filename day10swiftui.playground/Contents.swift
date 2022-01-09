7d520fa28b68c4483ddc5ba8edca66db7c67f1ce
ioschristian <christian.d.tisby@gmail.com>
2022-01-09 02:40:23 -0500

    completed day 10

--- day10swiftui.playground/Contents.swift
+++ day10swiftui.playground/Contents.swift
@@ -41,6 +41,31 @@ wings.printSummary()
 ///
 ///
 
+//struct Employee {
+//    let name: String
+//    var vacationAllocated = 14
+//    var vacationTaken = 0
+//
+//    var vacationRemaining: Int {
+//        get {
+//            vacationAllocated - vacationTaken
+//        }
+//
+//        set {
+//            vacationAllocated = vacationTaken + newValue
+//        }
+//
+//    }
+//
+//}
+//
+//var archer = Employee(name: "Sterling Archer", vacationAllocated: 14)
+//archer.vacationTaken += 4
+//archer.vacationRemaining = 5
+//print(archer.vacationRemaining)
+//
+
+
 struct Employee {
     let name: String
     var vacationAllocated = 14
@@ -54,15 +79,66 @@ struct Employee {
         set {
             vacationAllocated = vacationTaken + newValue
         }
-      
     }
-
 }
+
 
 var archer = Employee(name: "Sterling Archer", vacationAllocated: 14)
 archer.vacationTaken += 4
+print(archer.vacationAllocated)
 archer.vacationRemaining = 5
-print(archer.vacationRemaining)
+print(archer.vacationAllocated)
 
+struct Game {
+    var score = 0{
+        didSet {
+            print("Score is now \(score)")
+        }
+    }
+}
+
+
+
+var game = Game()
+game.score += 10
+game.score -= 3
+game.score += 1
+
+
+
+
+struct App {
+    var contacts = [String]() {
+        willSet {
+            print("Current value is: \(contacts)")
+            print("New value will be: \(newValue)")
+        }
+        
+        didSet {
+            print("There are now \(contacts.count) contacts.")
+            print("Old value was \(oldValue)")
+        }
+    }
+}
+
+
+var app = App()
+app.contacts.append("Adrian E")
+app.contacts.append("Allen W")
+app.contacts.append("Ish S")
+
+
+
+struct Player {
+    let name: String
+    let number: Int
+    
+    init(name: String, number: Int) {
+        self.name = name
+        self.number = number
+    }
+}
+
+let player = Player(name: "Megan R", number: 15)
 
 
