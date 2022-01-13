7ae55a4d58d86ea70a7be1befed543d46bdfd2ba
ioschristian <christian.d.tisby@gmail.com>
2022-01-12 21:20:25 -0500

    adding day 14

---
+++ day14swiftui.playground/Contents.swift
@@ -0,0 +1,108 @@
+import UIKit
+
+let opposites = ["Mario": "Wario", "Luigi": "Waluigi"]
+let peachOpposites = opposites["Peach"]
+
+if let marioOpposite = opposites["Mario"] {
+    print("Mario's opposite is \(marioOpposite)")
+}
+
+
+var username: String? = nil
+
+if let unwrappedName = username {
+    print("We got a user: \(unwrappedName)")
+} else {
+    print("The optional was empty.")
+}
+
+
+func square(number: Int) -> Int {
+    number * number
+}
+
+
+//andrei
+
+//Unwrapping optionals with Guard
+func printSquare(of number: Int?) {
+    guard let number = number else {
+        print("Missing input")
+        return
+    }
+    
+    print("\(number) x \(number) is \(number * number)")
+}
+
+var myVar: Int? = 3
+
+//if let unwrapped = myVar {
+//    print("Run if myVar has a value inside")
+//}
+//
+//guard let unwrapped = myVar else {
+//    print("Run if myVar doesn't have a value inside")
+//}
+
+
+let captains = [
+    "Enterprise": "Picard",
+    "Voyager": "Janeway",
+    "Defiant": "Sisko"
+]
+
+
+//let new = captains["Serenity"] ?? "N/A"
+
+let new = captains["Serenity", default: "N/A"]
+
+let tvShows = ["Archer", "Babylon 5", "Ted Lasso"]
+let favorite = tvShows.randomElement() ?? "None"
+
+
+struct Book {
+    let title: String
+    let author: String?
+}
+
+
+let book = Book(title: "Beowulf", author: nil)
+let author = book.author ?? "Anonymous"
+print(author)
+
+let input = ""
+let number = Int(input) ?? 0
+print(number)
+
+
+
+let names = ["Arya", "Bran", "Robb", "Sansa"]
+
+let chosen = names.randomElement()?.uppercased() ?? "No one"
+print("Next in line: \(chosen)")
+
+struct Book2 {
+    let title2: String
+    let author2: String?
+}
+
+var book2: Book2? = nil
+let author2 = book2?.author2?.first?.uppercased() ?? "A"
+print(author2)
+
+
+
+enum UserError: Error {
+    case badID, networkFailed
+}
+
+func getUser(id: Int) throws -> String {
+    throw UserError.networkFailed
+}
+
+if let user = try? getUser(id: 23) {
+    print("User: \(user)")
+}
+
+let user = (try? getUser(id: 23)) ?? "Anonymous"
+print(user)
