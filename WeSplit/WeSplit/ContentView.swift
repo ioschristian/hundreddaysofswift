0d5ecd06caa536b7a576092c7698183d0fcc6575
ioschristian <christian.d.tisby@gmail.com>
2022-01-13 23:37:01 -0500

    completed day 16

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -9,15 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Form {
-            Section {
-                Text("Hello, world!")
-            }
-            Section {
-                Text("Hello, world!")
-                Text("Hello, world!")
-            }
-        }
+        Text("Hello, world!")
+            .padding()
     }
 }
 
