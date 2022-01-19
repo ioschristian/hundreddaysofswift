6de6f065a8f3edb5a07602b54f4aed664e905f74
ioschristian <christian.d.tisby@gmail.com>
2022-01-18 23:32:55 -0500

    starting project 2 Guess The Flag

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Christian Tisby on 1/18/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
